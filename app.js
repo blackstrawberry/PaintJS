0322cfe9cbf95df4530b3baa7786594d7bcad649
김종한 <jong@BlackStrawberry.local>
2022-06-15 23:33:24 +0900

    Now We can chice color and draw

---
+++ app.js
@@ -0,0 +1,47 @@
+const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jscolor");
+
+// canvas.width = document.getElementsByClassName("canvas")[0].offsetWidth;
+// canvas.height = document.getElementsByClassName("canvas")[0].offsetHeight;
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
+
+let painting = false;
+function stopPainting(){
+  painting = false;
+}
+function startPainting(){
+  painting = true;
+}
+
+function onMouseMove(event){
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if(!painting){
+    ctx.beginPath();
+    ctx.moveTo(x,y);
+  }else{
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
+}
+function onMouseDown(event){
+  painting = true;
+}
+function handleColorClick(event){
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;  
+}
+if(canvas){
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener('mouseup', stopPainting);
+  canvas.addEventListener('mouseleave', stopPainting);
+}
+
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColorClick));
